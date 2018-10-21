54a31388f58d33b4269bb9b7ebb311d1266f5e99
Bartosz Boguniewicz <bartosz.boguniewicz@globallogic.com>
2018-10-21 18:58:19 +0200

    May the journey begin!

---
+++ core/src/main/java/org/globaldayofcoderetreat/gameoflive/core/Application.java
@@ -0,0 +1,8 @@
+package org.globaldayofcoderetreat.gameoflive.core;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
